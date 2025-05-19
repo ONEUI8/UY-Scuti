128c2e13e8814f935db9dfd93b8f104d1ddacbe6
oneui <oneui@linux.localdomain>
2025-05-19 11:21:11 +0800

    fixed

--- resources/languages/start_en.sh
+++ resources/languages/start_en.sh
@@ -1,31 +1,34 @@
 #!/bin/bash
 
 # Import various modules
-for script in "$(dirname "$0")/resources/module_codes/en"/*.sh \
+for script in "$(dirname "$0")/resources/module_codes/cn"/*.sh \
 	"$(dirname "$0")/resources/module_codes/general"/*.sh; do
 	source "$script"
 done
-source "$(dirname "$0")/resources/my_tools/nice_rom/only_modify_en.sh"
+source "$(dirname "$0")/resources/my_tools/nice_rom/only_modify.sh"
+
 super_sub_partitions_list="my_.*\.img|\
 mi_ext\.img|\
 odm\.img|odm_dlkm\.img|\
 product\.img|\
 system\.img|system_dlkm\.img|system_ext\.img|\
 vendor\.img|vendor_dlkm\.img"
-# Define tool and workspace directory paths
+
+# Define paths for tools and workspace directories
 TOOL_DIR="$(dirname "$0")/resources/my_tools"
 WORK_DIR="$(dirname "$0")/my_workspaces"
 
-# Define current workspace
+# Define current workspace variable
 current_workspace=""
 
+# Function to show main menu
 function show_main_menu {
 	clear
 	echo -e "\033[38;2;135;206;235m"
 	echo -e "                               "
 	echo -e "   ──────────────────────────"
 	echo -e "                             "
-	echo -e "           UY Scuti    "
+	echo -e "           UY Scuti  "
 	echo -e "                             "
 	echo -e "   ──────────────────────────"
 	tput sgr0
@@ -34,37 +37,38 @@ function show_main_menu {
 	echo -e "   [03] Delete Workspace\n"
 	echo -e "   [04] Change Language Settings\n"
 	echo -e "   [05] Exit Program\n"
-	echo -n "   Please select an operation: "
+	echo -n "   Please select an option: "
 }
 
-# Function to display workspace menu
+# Function to show workspace menu
 function show_workspace_menu {
 	echo -e "\033[38;2;135;206;235m"
 	echo -e "                               "
-	echo -e "   ──────────────────────────"
-	echo -e "                                      "
-	echo -e "            Workspace       "
-	echo -e "                                      "
-	echo -e "   ──────────────────────────"
+	echo -e "   ────────────────────────"
+	echo -e "                             "
+	echo -e "           Workspace        "
+	echo -e "                             "
+	echo -e "   ────────────────────────"
 	tput sgr0
 	echo -e "\n   [01] Extract Partition Files\n"
 	echo -e "   [02] Package Partition Files\n"
-	echo -e "   [03] SUPER Partition Package\n"
-	echo -e "   [04] One-Click Modify\n"
+	echo -e "   [03] Package SUPER Partition\n"
+	echo -e "   [04] One-click Modify\n"
 	echo -e "   [05] Build Flash Package\n"
 	echo -e "   [06] Return to Main Menu\n"
 	echo -e "   [07] Exit Program\n"
-	echo -n "   Please select an operation: "
+	echo -n "   Please select an option: "
 }
 
+# Function to create workspace
 function create_workspace {
 	while true; do
 		echo ""
-		echo -n "   Please enter the name of the new workspace: "
+		echo -n "   Please enter a name for the new workspace: "
 		read workspace
 		if [ -z "$workspace" ]; then
 			clear
-			echo -e "\n   You did not provide a valid input."
+			echo -e "\n   No valid input was entered."
 			continue
 		fi
 		if echo "$workspace" | grep -Pvq "^[a-zA-Z0-9_\-\.\p{Han}—\s]*$"; then
@@ -73,13 +77,13 @@ function create_workspace {
 		else
 			if [ -d "$WORK_DIR/$workspace" ]; then
 				echo "   Workspace $workspace already exists, no need to create."
-				echo -n "   Press any key to return to the main menu..."
+				echo -n "   Press any key to return to main menu..."
 				read -n 1
 				return
 			else
 				mkdir -p "$WORK_DIR/$workspace"
 				echo "   Workspace $workspace has been created."
-				echo -n "   Press any key to return to the main menu..."
+				echo -n "   Press any key to return to main menu..."
 				read -n 1
 				return
 			fi
@@ -87,11 +91,12 @@ function create_workspace {
 	done
 }
 
+# Function to select workspace
 function select_workspace {
 	local workspaces=("$WORK_DIR"/*)
 	if [ -z "$(ls -A "$WORK_DIR")" ]; then
 		echo -e "\n"
-		echo -n "   No available workspaces, press any key to return."
+		echo -n "   No available workspace, press any key to return."
 		read -n 1
 		return
 	fi
@@ -106,7 +111,7 @@ function select_workspace {
 				i=$((i + 1))
 			fi
 		done
-		echo -e "\n   [Q] Return to Main Menu\n"
+		echo -e "\n   [Q] Return to main menu\n"
 		echo -n "   Please enter the workspace number to select: "
 		read choice
 		if [[ "$choice" =~ ^[Qq]$ ]]; then
@@ -120,19 +125,20 @@ function select_workspace {
 				break
 			else
 				clear
-				echo -e "\n   The workspace number does not exist, please re-enter."
+				echo -e "\n   The workspace number does not exist, please try again."
 			fi
 		else
 			clear
-			echo -e "\n   Invalid input, please re-enter."
+			echo -e "\n   Invalid input, please try again."
 		fi
 	done
 }
 
+# Function to delete workspace
 function delete_workspace {
 	if [ -z "$(ls -A "$WORK_DIR")" ]; then
 		echo -e "\n"
-		echo -n "   No workspaces to delete, press any key to return."
+		echo -n "   No workspace to delete, press any key to return."
 		read -n 1
 		return
 	fi
@@ -147,8 +153,8 @@ function delete_workspace {
 				i=$((i + 1))
 			fi
 		done
-		echo -e "\n   [Q] Return to Main Menu\n"
-		echo -n "   Please enter the workspace number to delete: "
+		echo -e "\n   [Q] Return to main menu\n"
+		echo -n "   Please enter the number of the workspace to delete: "
 		read choice
 		if [[ "$choice" =~ ^[Qq]$ ]]; then
 			return
@@ -158,21 +164,21 @@ function delete_workspace {
 				rm -rf "$workspace"
 				find "$TOOL_DIR/boot_editor" -mindepth 1 ! -regex '^'"$TOOL_DIR/boot_editor"'/\(aosp\|bbootimg\|src\|tools\|gradlew\)\(/.*\)?$' -exec rm -rf {} \; 2>/dev/null
 				echo "   Workspace $(basename "$workspace") has been deleted."
-				echo -n "   Press any key to return to the main menu..."
+				echo -n "   Press any key to return to main menu..."
 				read -n 1
 				return
 			else
 				clear
-				echo -e "\n   The workspace number does not exist, please re-enter."
+				echo -e "\n   The workspace number does not exist, please try again."
 			fi
 		else
 			clear
-			echo -e "\n   Invalid input, please re-enter."
+			echo -e "\n   Invalid input, please try again."
 		fi
 	done
 }
 
-# Add new functionality in the workspace menu function
+# Add new features in workspace menu function
 function workspace_menu {
 	while true; do
 		clear
@@ -210,12 +216,13 @@ function workspace_menu {
 			;;
 		*)
 			clear
-			echo "   Invalid selection, please re-enter."
+			echo "   Invalid selection, please try again."
 			;;
 		esac
 	done
 }
 
+# One-click modify action
 function one_click_modify {
 	pushd . >/dev/null
 	local workspace_path=$(realpath "$WORK_DIR/$current_workspace")
@@ -224,10 +231,12 @@ function one_click_modify {
 	popd
 }
 
+# Remove unwanted Zone.Identifier files to keep directory clean
 function keep_clean {
 	find "$(dirname "$0")" -type f -name "*Zone.Identifier*" -exec rm -rf {} \;
 }
 
+# Main loop
 while true; do
 	clear
 	keep_clean
@@ -249,8 +258,8 @@ while true; do
 	4)
 		clear
 		echo -e "\n   [1] English\n"
-		echo -e "   [2] Chinese\n"
-		echo -n "   Please select a new language setting: "
+		echo -e "   [2] 中文\n"
+		echo -n "   Please select new language setting: "
 		read new_language
 		if [ "$new_language" = "1" ]; then
 			replace_script "start_en.sh"
@@ -259,7 +268,7 @@ while true; do
 			replace_script "start_cn.sh"
 			exec "$(dirname "$0")/start.sh"
 		else
-			echo "   Invalid selection, please re-enter."
+			echo "   Invalid selection, please try again."
 		fi
 		;;
 	5)
@@ -267,7 +276,7 @@ while true; do
 		exit 0
 		;;
 	*)
-		echo "   Invalid selection, please re-enter."
+		echo "   Invalid selection, please try again."
 		;;
 	esac
 done
